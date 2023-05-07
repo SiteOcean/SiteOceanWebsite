ca5c8b9dd5859355eb945d37cb0f8fcc8db29ac7
SiteOcean <tosathishsite@gmail.com>
2023-05-07 20:35:47 +0530

    tailwind imported in css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import Layout from './Layouts/Layout'
+import 'tailwindcss/tailwind.css';
+
 
 export default function App({ Component, pageProps }) {
   return  <Layout>
