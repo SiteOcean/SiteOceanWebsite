a37ad793311fea3719880070bab217b985bc8537
SiteOcean <tosathishsite@gmail.com>
2023-05-09 12:29:01 +0530

    TEST

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const EMAIL = process.env.EMAIL;
+const EMAIL_PASS = process.env.EMAIL_PASS
 const nextConfig = {
   reactStrictMode: true,
+  env: { EMAIL, EMAIL_PASS }
 }
 
 module.exports = nextConfig
